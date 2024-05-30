d92f38400735efa9251b90a1fbbbacf7a28a031a
Samet Ünverdi <sametunv@gmail.com>
2024-05-30 16:15:24 +0300

    Add project files.

---
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+
+namespace MvcKutuphane.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        DBKUTUPHANEEntities1 db = new DBKUTUPHANEEntities1();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+        public ActionResult KategoriEkle(TBLKATEGORI p)
+        {
+            db.TBLKATEGORI.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
