e75086b7ab3873d076e326e5990c501edfd405d6
Samet Ünverdi <sametunv@gmail.com>
2024-05-30 16:58:37 +0300

    Sql tablosu oluşturuldu ve diagram bağlandı. Daha sonra kategoriler oluşturulup ekle, sil ve güncelle butonları eklendi.

--- MvcKutuphane/Controllers/KategoriController.cs
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -16,11 +16,36 @@ namespace MvcKutuphane.Controllers
             var degerler = db.TBLKATEGORI.ToList();
             return View(degerler);
         }
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+        [HttpPost]
         public ActionResult KategoriEkle(TBLKATEGORI p)
         {
             db.TBLKATEGORI.Add(p);
             db.SaveChanges();
             return View();
+        }
+        public ActionResult KategoriSil(int id) 
+        {
+            var kategori = db.TBLKATEGORI.Find(id);
+            db.TBLKATEGORI.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id) 
+        {
+            var ktg = db.TBLKATEGORI.Find(id);
+            return View("KategoriGetir",ktg);
+        }
+        public ActionResult KategoriGuncelle(TBLKATEGORI p) 
+        {
+            var ktg = db.TBLKATEGORI.Find(p.ID);
+            ktg.AD = p.AD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
